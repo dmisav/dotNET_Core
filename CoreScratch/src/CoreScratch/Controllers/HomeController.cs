7e8fe925f489f3c452409224c4fe97ce30c5c43e
dmisav <savchenko.di@gmail.com>
2017-04-05 05:55:48 +0300

    added CoreScratch project

---
+++ CoreScratch/src/CoreScratch/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using CoreScratch.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreScratch.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var employee = new Employee() { ID=1, Email="11@aaa.cc", Name="Name", Salary = 100 };
+            return View(employee);
+        }
+    }
+}
